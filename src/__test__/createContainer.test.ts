9a0cd4bfd9c596b75982e1a701e39f08c07fbf7c
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-08-26 16:06:41 +0700

    restructure file

---
+++ src/__test__/createContainer.test.ts
@@ -0,0 +1,32 @@
+// @ts-nocheck
+// eslint-disable-next-line max-classes-per-file
+import InstanceOption from '../definitions/InstanceOption';
+import { createContainer } from '../createContainer';
+
+describe('createContainer', () => {
+  it('should create container correctly when given single instance option', () => {
+    const instanceOption: InstanceOption = {
+      Class: class Car {},
+    };
+
+    const container = createContainer(instanceOption);
+
+    expect(container.instances.Car).toBeInstanceOf(Object);
+  });
+
+  it('should create container correctly when given multiple instance options', () => {
+    const instanceOptions: InstanceOption[] = [
+      {
+        Class: class Car {},
+      },
+      {
+        Class: class Engine {},
+      },
+    ];
+
+    const container = createContainer(instanceOptions);
+
+    expect(container.instances.Engine).toBeInstanceOf(Object);
+    expect(container.instances.Car).toBeInstanceOf(Object);
+  });
+});
